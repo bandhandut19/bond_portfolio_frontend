06238ef0869f522e65da36b0e03f5d0aee630c2c
bandhandut19 <bondon21081@gmail.com>
2024-11-22 23:44:44 +0600

    dummy projects added with images

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,7 +7,11 @@ const projects = [
       "A web application to manage projects, tasks, and teams efficiently.",
     github_url: "https://github.com/username/project-management-system",
     live_url: "https://www.projectmanagementsystem.com",
-    image: "https://i.postimg.cc/J0b6Fgq8/project1.png",
+    image: "https://i.postimg.cc/CLYDXGct/best-project-design.png",
+    used_technologies: {
+      frontend: ["React", "TailwindCSS", "Redux"],
+      backend: ["Node.js", "Express", "MongoDB"],
+    },
   },
   {
     name: "E-Commerce Store",
@@ -15,7 +19,11 @@ const projects = [
       "An online marketplace to buy and sell products with integrated payment systems.",
     github_url: "https://github.com/username/e-commerce-store",
     live_url: "https://www.ecommercestore.com",
-    image: "https://i.postimg.cc/hjqmFgH1/project2.png",
+    image: "https://i.postimg.cc/CLYDXGct/best-project-design.png",
+    used_technologies: {
+      frontend: ["React", "Bootstrap", "Redux"],
+      backend: ["Node.js", "Express", "MySQL"],
+    },
   },
   {
     name: "Personal Portfolio",
@@ -23,7 +31,11 @@ const projects = [
       "A personal website showcasing my skills, projects, and blogs.",
     github_url: "https://github.com/username/personal-portfolio",
     live_url: "https://www.personalportfolio.com",
-    image: "https://i.postimg.cc/PqT8sN7X/project3.png",
+    image: "https://i.postimg.cc/CLYDXGct/best-project-design.png",
+    used_technologies: {
+      frontend: ["HTML", "CSS", "JavaScript", "React"],
+      backend: ["None"],
+    },
   },
 ];
 
@@ -33,7 +45,7 @@ const Projects = () => {
       <div className="bg-[#AB886D] text-center lg:text-5xl text-2xl py-10 h-1/4 rounded-b-full text-[#E4E0E1] pt-10 mb-10">
         <span className="font-extrabold">Projects</span>
       </div>
-      <div className="grid grid-cols-2 gap-5 px-5">
+      <div className="grid lg:grid-cols-2 gap-5 px-5">
         {projects.map((project, index) => (
           <div
             key={index}
@@ -45,13 +57,40 @@ const Projects = () => {
             <div className="card-body">
               <h2 className="card-title">{project.name}</h2>
               <p>{project.description}</p>
-              <div className="grid grid-cols-2 gap-5 text-center font-bold">
-                <span className="bg-[#D6C0B3] text-black rounded-full">
-                  <Link to={project.github_url}>Github Repo</Link>
+              <div className="grid grid-cols-2 gap-5 text-center font-bold cursor-pointer">
+                <span className="bg-[#D6C0B3] text-black rounded-full py-2">
+                  <Link to={project.github_url} target="_blank">
+                    Github Repo
+                  </Link>
                 </span>
-                <span className="bg-[#D6C0B3] text-black rounded-full">
-                  <Link to={project.live_url}>Live Link</Link>
+                <span className="bg-[#D6C0B3] text-black rounded-full py-2">
+                  <Link to={project.live_url} target="_blank">
+                    Live Link
+                  </Link>
                 </span>
+              </div>
+              <div>
+                <h1 className="text-center bg-[#D6C0B3] text-[#493628] py-2 font-extrabold">
+                  Techonlogies Used
+                </h1>
+                <div className="grid grid-cols-2 gap-5 text-center text-[#493628] py-1">
+                  <div className="bg-[#E4E0E1] py-2">
+                    <span className="font-bold">Frontend</span>
+                    <div>
+                      {project.used_technologies.frontend.map((tech, index) => (
+                        <p key={index}>{tech}</p>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="bg-[#E4E0E1] py-2">
+                    <span className="font-bold">Backend</span>
+                    <div>
+                      {project.used_technologies.backend.map((tech, index) => (
+                        <p key={index}>{tech}</p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
